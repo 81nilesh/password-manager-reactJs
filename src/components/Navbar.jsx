8e3f9b1376e386ec8f3ec97baeee94b33001bcd5
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-06-03 14:53:41 +0530

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Nave Bar</h1>
-        </div>
+        <nav className='bg-purple-200 flex items-center px-14 justify-between h-12'>
+            <div className="logo font-bold">Passop</div>
+            <ul>
+                <li className='flex gap-12'>
+                    <a className='hover:font-bold' href="/">Home</a>
+                    <a className='hover:font-bold' href="#">About</a>
+                    <a className='hover:font-bold' href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
