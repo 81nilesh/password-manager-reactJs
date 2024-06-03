3b902a5404dd9720f274e38be81f92adfec7192a
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-06-03 15:17:24 +0530

    navbar correction

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -4,7 +4,18 @@ const Manager = () => {
     return (
         <>
             <div class="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
-            <div></div>
+
+            <div className="mx-auto bg-slate-50 mycontainer">
+                <h1>PassOP</h1>
+                <p>Your Own Password Manager</p>
+                <div className='text-white flex flex-col p-4'>
+                    <input className='rounded-full' type="text" name='' id='' />
+                    <div className="flex">
+                        <input type="text" />
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className='bg-purple-200 flex items-center px-14 justify-between h-12'>
-            <div className="logo font-bold">Passop</div>
-            <ul>
-                <li className='flex gap-12'>
-                    <a className='hover:font-bold' href="/">Home</a>
-                    <a className='hover:font-bold' href="#">About</a>
-                    <a className='hover:font-bold' href="#">Contact</a>
-                </li>
-            </ul>
+        <nav className='bg-slate-800 text-white'>
+            <div className="mycontainer flex justify-between items-center px-14 py-5  h-12">
+                <div className="logo font-bold">
+                    <span className='text-green-700'> &lt;</span>
+                    PassOP
+                    <span className='text-green-700'>/ &gt;</span>
+
+                </div>
+                <ul>
+                    <li className='flex gap-12'>
+                        <a className='hover:font-bold' href="/">Home</a>
+                        <a className='hover:font-bold' href="#">About</a>
+                        <a className='hover:font-bold' href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
